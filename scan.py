e3c6ae606cdcdf63d847fbfa9290672473cc21fe
Kedy Coder <101480197+kedyc0der@users.noreply.github.com>
2022-04-02 03:30:25 +0300

    Create scan.py

---
+++ scan.py
@@ -0,0 +1,19 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(5)
+print("""
+       Kedy Coder 
+
+instagram: @kedy.coder
+Youtube : Kedy Coder
+
+    - PORT SCANNER -
+      """)
+host = input("LÜTFEN İP ADRESİNİ GİRİNİZ : ")
+port = int(input("TARATILACAK PORT ADRESİNİ GİRİNİZ : "))
+def portScanner(port):
+    if s.connect_ex((host, port)):
+        print( "BU PORT KAPALI")
+    else:
+        print("BU PORT AÇIK")
+portScanner(port)
